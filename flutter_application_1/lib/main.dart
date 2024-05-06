f5371c61c7cdcae4119bf4b2d2c6dc2ef8cbafbc
Fhriii <muhammadfahrrri@gmail.com>
2024-05-06 09:17:45 +0700

    sec

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -3,19 +3,35 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(MyFirstApp());
 }
-
 class MyFirstApp extends StatelessWidget{
-@override
+  List<Color> mycolor = [Colors.red,Colors.blue,Colors.yellow,Colors.black];
+  List<Widget> num=List.generate(90, (index) => Text(index.toString(),
+    style: TextStyle(
+      color: Colors.red,
+      fontSize: 10 +double.parse(index.toString())),
+  ));
+
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text("Ini child text"),),
-        appBar: AppBar(title: Center(child:Text("hlo hai") ,)
-        )
-      
-      ,)
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(
+            child: Text("My  App"))
+        ,),
+
+        body:ListView(
+          children: num,
+          
+          ),
+        
+    ),
 
     );
+    
   }
+  
 }
