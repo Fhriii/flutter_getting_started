8f8c26d53d72f705f1b351abbc47ceb0aed72854
Fhriii <muhammadfahrrri@gmail.com>
2024-05-16 08:19:34 +0700

    stattless widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter/widgets.dart';
 
 void main(List<String> args) {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget{
+class MyApp extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
@@ -12,21 +14,63 @@ class myApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 9, 69, 117),
           title: Center(
-            child: Text("My App"),
+            child: Text("Yok Bisa Yok!!"),
           ),
           
           ),
-          body: Center(
-            child: Container(
-              width: 300,
-              height: 300,
-              color: Colors.blue,
-
-              child: Image(image: AssetImage("images/download.jpeg"),),
-            ),
+          body: ListView(
+            children: [
+              listcontact(
+                imageUrl: "images/download.jpeg",
+                title: faker.person.name(),
+                subtitle: faker.internet.email(),
+                trailing: "10.00 PM",
+              ),
+              listcontact(
+                imageUrl: "images/download.jpeg",
+                title: faker.person.name(),
+                subtitle: faker.internet.email(),
+                trailing: "10.00 PM",
+              ),
+              listcontact(
+                imageUrl: "images/download.jpeg",
+                title: faker.person.name(),
+                subtitle: faker.internet.email(),
+                trailing: "10.00 PM",
+              ),
+              listcontact(
+                imageUrl: "images/download.jpeg",
+                title: faker.person.name(),
+                subtitle: faker.internet.email(),
+                trailing: "10.00 PM",
+              ),
+            ],
           ),
         ),
     );
   }
 }
+
+class listcontact extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+  final String trailing;
+
+  listcontact({ required this.imageUrl,required this.title,required this.subtitle,required this.trailing});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(backgroundImage:AssetImage(imageUrl) ,),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Text(trailing),
+      
+    
+    );
+  }
+}
+
